dff8c3a3e9aa9c693c0f330851bd3a166574978a
Melinda Lu <lumel@berkeleyprep.org>
2019-02-04 15:14:11 -0500

    all pass

--- Homework12.java
+++ Homework12.java
@@ -6,19 +6,20 @@ import java.awt.geom.RectangularShape;
 import java.util.ArrayList;
 
 public class Homework12 {
+	private ArrayList<RectangularShape> list;
 
 	/* Finish the constructor and create any necessary instance
 	 * variables. The constructor should create and save an
 	 * ArrayList of RectangularShape values
 	 */
 	public Homework12() {
-
+		list = new ArrayList<RectangularShape>();
 	}
 
 	/* Adds the parameter s to the ArrayList
 	 */
 	public void addShape(RectangularShape s) {
-
+		list.add(s);
 	}
 
 	/* Returns the number of RectangularShape objects
@@ -26,6 +27,13 @@ public class Homework12 {
 	 * parameter r
 	 */
 	public int problem1(Rectangle2D r) {
+		int num = 0;
+			for (int i = 0; i < list.size(); i++) {
+				if (list.get(i).intersects(r)) {
+					num++;
+				}
+			}
+			return num;
 
 	}
 
@@ -34,21 +42,41 @@ public class Homework12 {
 	 * parameter p
 	 */
 	public int problem2(Point2D p) {
-
+		int num = 0;
+			for (int i = 0; i < list.size(); i++) {
+				if (!(list.get(i).contains(p))) {
+					num++;
+				}
+			}
+			return num;
 	}
 
 	/* Returns the number of Ellipse2D objects that
 	 * are in the ArrayList
 	 */
 	public int problem3() {
-
+		int num = 0;
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i) instanceof Ellipse2D) {
+				num++;
+			}
+		}
+		return num;
 	}
 
 	/* Returns true if any of the Rectangle2D objects
 	 * in the ArrayList intersects the parameter Line2D
 	 */
 	public boolean problem4(Line2D l) {
-
+		boolean check = false;
+			for (int i = 0; i < list.size(); i++) {
+				if (list.get(i) instanceof Rectangle2D) {
+					if (((Rectangle2D) (list.get(i))).intersectsLine(l)) {
+						check = true;
+					}
+				}
+			}
+			return check;
 	}
 
 	/* Return the total area of the RectangularShape objects in
@@ -60,7 +88,16 @@ public class Homework12 {
 	 * counts for both (i.e. do not account for intersection)
 	 */
 	public double problem5() {
-
+		double area = 0;
+			for(int i = 0; i < list.size(); i++) {
+				RectangularShape shape = list.get(i);
+				if(list.get(i) instanceof Rectangle2D) {
+					area += (shape.getHeight() * shape.getWidth());
+				} else if(list.get(i) instanceof Ellipse2D) {
+						area += (Math.PI) * (shape.getHeight() / 2) * (shape.getWidth() / 2);
+					}
+			}
+			return area;
 	}
 
 	public static void main(String[] args) {
